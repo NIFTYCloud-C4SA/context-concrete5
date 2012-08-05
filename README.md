2109858ec0802f6b0b313f05cf925bf162636adf
ysaotome <y@sotm.jp>
2012-08-05 23:57:23 +0900

    Concrete5 for NIFTYCloud C4SA First commit

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 context-concrete5
 =================
 
-concrete5のニフティクラウドC4SA用コンテクスト http://concrete5-japan.org/
+concrete5のニフティクラウドC4SA用コンテクスト
+
+* [concrete5 公式](http://www.concrete5.org/)
+* [concrete5 Japan 日本語公式サイト](http://concrete5-japan.org/)
+* [ニフティクラウドC4SA](http://c4sa.nifty.com/)
---
+++ makecontext.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+BIN_CURL="/usr/bin/curl -L"
+BIN_GIT="/usr/bin/git"
+BIN_MV="/bin/mv"
+BIN_RM="/bin/rm -rf"
+BIN_TAR="/usr/bin/tar"
+BIN_UNZIP="/usr/bin/unzip"
+BIN_MKDIR="/bin/mkdir"
+
+URL_REPO="https://github.com/concrete5japan/concrete5.git"
+URL_ARCHIVE="https://github.com/downloads/concrete5japan/concrete5/concrete5.5.2.1.ja.zip"
+
+PATH_WORKDIR="./workdir"
+
+## make context
+${BIN_MKDIR} ${PATH_WORKDIR}
+
+#${BIN_GIT} clone ${URL_REPO} ${PATH_WORKDIR}/tmp_concrete5
+
+${BIN_CURL} -o ${PATH_WORKDIR}/tmp_concrete5.zip ${URL_ARCHIVE}
+${BIN_UNZIP} ${PATH_WORKDIR}/tmp_concrete5.zip -d ${PATH_WORKDIR}
+${BIN_MV} ${PATH_WORKDIR}/concrete5*/* ./contexts/krm/public_html/
+${BIN_RM} ${PATH_WORKDIR}
+
+## create archive
+${BIN_TAR} czf ./contexts/krm/web_root.tar.gz ./contexts/krm/public_html
+${BIN_RM} ./contexts/krm/public_html
+${BIN_TAR} czf ./contexts/krm.tar.gz ./contexts/krm
+${BIN_RM} ./contexts/krm/
+
+## end
+
+
